6189e505eef7b5333d1c046e3b4b4f18a9bce3e6
Pierre Lang <couseragit@pierre-lang.info>
2025-05-28 12:27:27 -0400

    task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -20,20 +20,75 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
-
+// Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  let book = books[isbn]  //assuming the isbn is the books object key at level 1. Would have liked a property...
+  let review = req.body.review;
+  if (book) {
+        if (!book.reviews) book.reviews = {};
+        let reviews = book.reviews;
+        let username = req.session.authorization['username'] //no test of existence needed, we are logged in
+        reviews[username] = review
+        return res.send("Review by user " +  username + " for book " + isbn + " " + book.title + " added/modified")
+  } else {
+        return res.status(404).send("Book isbn " + isbn + " not found");
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+let isbn = req.params.isbn
+    let book = books[isbn]  //assuming the isbn is the books object key at level 1. Would have liked a property...
+    let review = req.body.review;
+    if (book) {
+        if (!book.reviews) book.reviews = {};
+        let reviews = book.reviews;
+        let username = req.session.authorization['username'] //no test of existence needed, we are logged in
+        delete reviews[username];
+        return res.send("Review by user " +  username + " for book " + isbn + " " + book.title + " deleted")
+    } else {
+        return res.status(404).send("Book isbn " + isbn + " not found");
+    }
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
