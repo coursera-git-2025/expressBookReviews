f9be17d8ff6749d3343fffcb1f62f2012a5b60e1
Pierre Lang <couseragit@pierre-lang.info>
2025-05-28 12:59:23 -0400

    task 14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,43 +26,88 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(books);
+  let p = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log("insettimeout")
+        let r = res.send(books)
+        resolve(r)
+    },2000)
+  })
+  p.then(()=>{console.log("promise resolved")})
+  return p;
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   let isbn = req.params.isbn
-  let book = books[isbn]  //assuming the isbn is the books object key at level 1. Would have liked a property...
-  if (book) {
-        return res.send(book);
-  } else {
-        return res.status(404).send("Book isbn " + isbn + " not found");
-  }
-  
+  let p = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log("insettimeout")
+        let book = books[isbn]  //assuming the isbn is the books object key at level 1. Would have liked a property...
+        let r;
+        if (book) {
+                r = res.send(book);
+        } else {
+                r = res.status(404).send("Book isbn " + isbn + " not found");
+        }
+        resolve(r) 
+    },2000)
+  })
+  p.then(()=>{console.log("promise resolved")})
+  return p;  //yes, I know it's not really useful to return anything here, but well, at least the info would be transmitted
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let author = req.params.author
-    for (let key in books) {
-        let book = books[key];
-        if (book.author==author) {
-            return res.send(book)   //Yeah...it should really return an array, but the task says "THE BOOK" (not a plural) so returning the first one
-        }
-    }
-    return res.status(404).send("Book having author " + author + " not found");
+    let p = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("insettimeout")
+            var bookarr = [];
+            for (let key in books) {
+                let book = books[key];
+                if (book.author==author) {
+                    bookarr.push(book)   
+                }
+            }
+            let r;
+            if (bookarr.length==0) {
+                r = res.status(404).send("Book having author " + author + " not found")
+            } else {
+                r = res.send(bookarr[0])//Yeah...it should really return an array, but the task says "the BOOK" (not a plural) so returning the first one
+            }
+            
+            resolve(r) 
+        },2000)
+      })
+      p.then(()=>{console.log("promise resolved")})
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   let title = req.params.title
-  for (let key in books) {
-      let book = books[key];
-      if (book.title==title) {
-          return res.send(book)   
-      }
-  }
-  return res.status(404).send("Book titled " + title + " not found");
+  let p = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log("insettimeout")
+        var bookarr = [];
+        for (let key in books) {
+            let book = books[key];
+            if (book.title==title) {
+                bookarr.push(book)   
+            }
+        }
+        let r;
+        if (bookarr.length==0) {
+            r = res.status(404).send("Book titled " + title + " not found");
+        } else {
+            r = res.send(bookarr[0])//Yeah...it should really return an array, but the task says "the BOOK" (not a plural) so returning the first one
+        }
+        
+        resolve(r) 
+    },2000)
+  })
+  p.then(()=>{console.log("promise resolved")})
 });
 
 //  Get book review
